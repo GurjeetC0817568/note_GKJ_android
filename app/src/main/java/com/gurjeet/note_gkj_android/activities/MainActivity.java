0de7eed2766f2ae8e7abba850dd92d5f920a446c
Gurjeet Kj <gurjeet-kj>
2021-10-09 16:07:33 -0400

    Added SplashActivity with front page design

--- app/src/main/java/com/gurjeet/note_gkj_android/activities/MainActivity.java
+++ app/src/main/java/com/gurjeet/note_gkj_android/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.gurjeet.note_gkj_android;
+package com.gurjeet.note_gkj_android.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.gurjeet.note_gkj_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/gurjeet/note_gkj_android/activities/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.gurjeet.note_gkj_android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.gurjeet.note_gkj_android.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final int SPLASH_DISPLAY_LENGTH = 8000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                startActivity(intent);//moving to second activity page
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
