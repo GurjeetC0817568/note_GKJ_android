19ecfc41cf780d2f6d8590b17f1426e4ab1b0605
Gurjeet Kj <gurjeet-kj>
2021-10-09 02:10:40 -0400

    NoteRoomDB file added under utilities package

---
+++ app/src/main/java/com/gurjeet/note_gkj_android/utilities/NoteRoomDB.java
@@ -0,0 +1,59 @@
+package com.gurjeet.note_gkj_android.utilities;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.gurjeet.note_gkj_android.data.NoteDao;
+import com.gurjeet.note_gkj_android.model.Category;
+import com.gurjeet.note_gkj_android.model.Note;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Category.class, Note.class}, version = 1, exportSchema = false)
+public abstract class NoteRoomDB extends RoomDatabase {
+    public abstract NoteDao noteDao();
+
+    private static volatile NoteRoomDB INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+
+    // executor service to do tasks in background thread
+    public static final ExecutorService databaseWriteExecutor
+            = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    public static NoteRoomDB getInstance(final Context context){
+        if(INSTANCE == null){
+            synchronized (NoteRoomDB.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NoteRoomDB.class, "note_gkj_android")
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static final RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
+                    databaseWriteExecutor.execute(() -> {
+                        NoteDao noteDao = INSTANCE.noteDao();
+                        noteDao.deleteAll();
+
+                        Category cat = new Category("Shopping");
+                        noteDao.insertCategory(cat);
+                        //int id=noteDao.getCatId();
+                        //Log.i(TAG, "Category Id " + position);
+
+                    });
+                }
+            };
+}
